265aa93237320ed1b83a785e6154a227793db5a2
patrycjamoss <patrycjamoss@gmail.com>
2022-02-16 12:06:02 +0100

    nazwy

--- js/main.js
+++ js/main.js
@@ -1,8 +1,7 @@
-let firstNumber = 1 ;
+let firstNumber = 1;
 let secondNumber = 2;
-
-function add(firstNumber,secondNumber){
-    let result = firstNumber+secondNumber;
+function add(numb1, numb2) {
+    let result = numb1 + numb2;
     return result;
 }
-console.log(add (1, 2))
+console.log(add(firstNumber, secondNumber))
